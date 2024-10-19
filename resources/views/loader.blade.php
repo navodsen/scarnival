90af485682962d42da441f5e92689e438c0a293c
Navod Senarathne <navodsenarathne@gmail.com>
2024-10-19 16:14:27 +0530

    Initial commit

---
+++ resources/views/loader.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Scarnival - Agastra Events</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @vite('resources/css/app.css')
+
+    </head>
+    <body class="font-sans antialiased bg-gradient-to-b from-zinc-900 to-zinc-950 text-white/50">
+        <div class="flex h-screen">
+            <img src="images/lantern.png" class="mx-auto w-20 my-auto lg:w-32">
+        </div>    
+    </body>
+</html>
